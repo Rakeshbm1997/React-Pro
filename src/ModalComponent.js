010937461449d0f70f9823184695eea143dacfae
RAKESHBM <bmrakesh7@gmail.com>
2023-12-08 14:45:34 +0530

    updated

--- src/ModalComponent.js
+++ src/ModalComponent.js
@@ -119,80 +119,80 @@ const ModalComponent = ({ showModal, handleClose }) => {
        <Form.Check name='inlineCheck' id='inlineCheckbox1' value='option1'className="d-inline-block float-end me-5 ms-5" inline />
       <Form.Check name='inlineCheck' id='inlineCheckbox2' value='option2' className="d-inline-block float-end me-2 ms-5"  inline /> 
       <Form.Check name='inlineCheck' id='inlineCheckbox3' value='option3' className="d-inline-block float-end me-2 ms-5" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox1' value='option1' className="d-inline-block float-end me-3 ms-5" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox2' value='option2' className="d-inline-block float-end me-4 ms-2" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox3' value='option3' className='float-end me-5 ' inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox4' value='option1' className="d-inline-block float-end me-3 ms-5" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox5' value='option2' className="d-inline-block float-end me-4 ms-2" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox6' value='option3' className='float-end me-5 ' inline />
       </div>
 
 
      <div>
     <span>Leaves</span>
-       <Form.Check name='inlineCheck' id='inlineCheckbox1' value='option1'className="d-inline-block float-end me-5 ms-5" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox2' value='option2' className="d-inline-block float-end me-2 ms-5"  inline /> 
-      <Form.Check name='inlineCheck' id='inlineCheckbox3' value='option3' className="d-inline-block float-end me-2 ms-5" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox1' value='option1' className="d-inline-block float-end me-3 ms-5" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox2' value='option2' className="d-inline-block float-end me-4 ms-2" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox3' value='option3' className='float-end me-5 ' inline />
+       <Form.Check name='inlineCheck' id='inlineCheckbox7' value='option1'className="d-inline-block float-end me-5 ms-5" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox8' value='option2' className="d-inline-block float-end me-2 ms-5"  inline /> 
+      <Form.Check name='inlineCheck' id='inlineCheckbox9' value='option3' className="d-inline-block float-end me-2 ms-5" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox10' value='option1' className="d-inline-block float-end me-3 ms-5" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox11' value='option2' className="d-inline-block float-end me-4 ms-2" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox12' value='option3' className='float-end me-5 ' inline />
       </div>
 
       <div>
     <span>Client</span>
-       <Form.Check name='inlineCheck' id='inlineCheckbox1' value='option1'className="d-inline-block float-end me-5 ms-5" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox2' value='option2' className="d-inline-block float-end me-2 ms-5"  inline /> 
-      <Form.Check name='inlineCheck' id='inlineCheckbox3' value='option3' className="d-inline-block float-end me-2 ms-5" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox1' value='option1' className="d-inline-block float-end me-3 ms-5" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox2' value='option2' className="d-inline-block float-end me-4 ms-2" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox3' value='option3' className='float-end me-5 ' inline />
+       <Form.Check name='inlineCheck' id='inlineCheckbox13' value='option1'className="d-inline-block float-end me-5 ms-5" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox14' value='option2' className="d-inline-block float-end me-2 ms-5"  inline /> 
+      <Form.Check name='inlineCheck' id='inlineCheckbox15' value='option3' className="d-inline-block float-end me-2 ms-5" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox16' value='option1' className="d-inline-block float-end me-3 ms-5" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox17' value='option2' className="d-inline-block float-end me-4 ms-2" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox18' value='option3' className='float-end me-5 ' inline />
       </div>
 
       <div>
     <span>Project</span>
-       <Form.Check name='inlineCheck' id='inlineCheckbox1' value='option1'className="d-inline-block float-end me-5 ms-5" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox2' value='option2' className="d-inline-block float-end me-2 ms-5"  inline /> 
-      <Form.Check name='inlineCheck' id='inlineCheckbox3' value='option3' className="d-inline-block float-end me-2 ms-5" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox1' value='option1' className="d-inline-block float-end me-3 ms-5" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox2' value='option2' className="d-inline-block float-end me-4 ms-2" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox3' value='option3' className='float-end me-5 ' inline />
+       <Form.Check name='inlineCheck' id='inlineCheckbox19' value='option1'className="d-inline-block float-end me-5 ms-5" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox20' value='option2' className="d-inline-block float-end me-2 ms-5"  inline /> 
+      <Form.Check name='inlineCheck' id='inlineCheckbox21' value='option3' className="d-inline-block float-end me-2 ms-5" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox22' value='option1' className="d-inline-block float-end me-3 ms-5" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox23' value='option2' className="d-inline-block float-end me-4 ms-2" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox24' value='option3' className='float-end me-5 ' inline />
       </div>
 
       <div>
     <span>Task</span>
-       <Form.Check name='inlineCheck' id='inlineCheckbox1' value='option1'className="d-inline-block float-end me-5 ms-5" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox2' value='option2' className="d-inline-block float-end me-2 ms-5"  inline /> 
-      <Form.Check name='inlineCheck' id='inlineCheckbox3' value='option3' className="d-inline-block float-end me-2 ms-5" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox1' value='option1' className="d-inline-block float-end me-3 ms-5" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox2' value='option2' className="d-inline-block float-end me-4 ms-2" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox3' value='option3' className='float-end me-5 ' inline />
+       <Form.Check name='inlineCheck' id='inlineCheckbox25' value='option1'className="d-inline-block float-end me-5 ms-5" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox26' value='option2' className="d-inline-block float-end me-2 ms-5"  inline /> 
+      <Form.Check name='inlineCheck' id='inlineCheckbox27' value='option3' className="d-inline-block float-end me-2 ms-5" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox28' value='option1' className="d-inline-block float-end me-3 ms-5" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox29' value='option2' className="d-inline-block float-end me-4 ms-2" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox30' value='option3' className='float-end me-5 ' inline />
       </div>
 
       <div>
     <span>Charts</span>
-       <Form.Check name='inlineCheck' id='inlineCheckbox1' value='option1'className="d-inline-block float-end me-5 ms-5" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox2' value='option2' className="d-inline-block float-end me-2 ms-5"  inline /> 
-      <Form.Check name='inlineCheck' id='inlineCheckbox3' value='option3' className="d-inline-block float-end me-2 ms-5" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox1' value='option1' className="d-inline-block float-end me-3 ms-5" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox2' value='option2' className="d-inline-block float-end me-4 ms-2" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox3' value='option3' className='float-end me-5 ' inline />
+       <Form.Check name='inlineCheck' id='inlineCheckbox31' value='option1'className="d-inline-block float-end me-5 ms-5" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox32' value='option2' className="d-inline-block float-end me-2 ms-5"  inline /> 
+      <Form.Check name='inlineCheck' id='inlineCheckbox33' value='option3' className="d-inline-block float-end me-2 ms-5" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox34' value='option1' className="d-inline-block float-end me-3 ms-5" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox35' value='option2' className="d-inline-block float-end me-4 ms-2" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox36' value='option3' className='float-end me-5 ' inline />
       </div>
 
       <div>
     <span>Assets</span>
-       <Form.Check name='inlineCheck' id='inlineCheckbox1' value='option1'className="d-inline-block float-end me-5 ms-5" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox2' value='option2' className="d-inline-block float-end me-2 ms-5"  inline /> 
-      <Form.Check name='inlineCheck' id='inlineCheckbox3' value='option3' className="d-inline-block float-end me-2 ms-5" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox1' value='option1' className="d-inline-block float-end me-3 ms-5" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox2' value='option2' className="d-inline-block float-end me-4 ms-2" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox3' value='option3' className='float-end me-5 ' inline />
+       <Form.Check name='inlineCheck' id='inlineCheckbox37' value='option1'className="d-inline-block float-end me-5 ms-5" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox38' value='option2' className="d-inline-block float-end me-2 ms-5"  inline /> 
+      <Form.Check name='inlineCheck' id='inlineCheckbox39' value='option3' className="d-inline-block float-end me-2 ms-5" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox40' value='option1' className="d-inline-block float-end me-3 ms-5" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox41' value='option2' className="d-inline-block float-end me-4 ms-2" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox42' value='option3' className='float-end me-5 ' inline />
       </div>
 
       <div>
     <span>Timing Sheets</span>
-       <Form.Check name='inlineCheck' id='inlineCheckbox1' value='option1'className="d-inline-block float-end me-5 ms-5" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox2' value='option2' className="d-inline-block float-end me-2 ms-5"  inline /> 
-      <Form.Check name='inlineCheck' id='inlineCheckbox3' value='option3' className="d-inline-block float-end me-2 ms-5" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox1' value='option1' className="d-inline-block float-end me-3 ms-5" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox2' value='option2' className="d-inline-block float-end me-4 ms-2" inline />
-      <Form.Check name='inlineCheck' id='inlineCheckbox3' value='option3' className='float-end me-5 ' inline />
+       <Form.Check name='inlineCheck' id='inlineCheckbox43' value='option1'className="d-inline-block float-end me-5 ms-5" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox44' value='option2' className="d-inline-block float-end me-2 ms-5"  inline /> 
+      <Form.Check name='inlineCheck' id='inlineCheckbox45' value='option3' className="d-inline-block float-end me-2 ms-5" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox46' value='option1' className="d-inline-block float-end me-3 ms-5" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox47' value='option2' className="d-inline-block float-end me-4 ms-2" inline />
+      <Form.Check name='inlineCheck' id='inlineCheckbox48' value='option3' className='float-end me-5 ' inline />
       </div>     
  
     </Form>
